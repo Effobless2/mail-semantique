9de78ee19fbfc4993665240ce818cafbacd50848
Maxime Deboffle <maxime.deboffle@expaceo.com>
2020-12-02 19:14:42 +0100

    change views to class components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
-export default {
-  name: 'Home',
+@Component({
   components: {
     HelloWorld
   }
+})
+export default class Home extends Vue {
 }
 </script>
